ed7b75923d4e7712a380ed724f95eda5a346dab9
Simon Shields <simon@lineageos.org>
2017-06-26 13:20:34 +1000

    device-deps: fix -caf branches and remove the COMMON_DEVICES array
    * kernel branches with a suffix are now represent by appending the suffix to
    the kernel's name: e.g. 'android_kernel_lge_hammerhead' with a branch of
    'cm-14.1-caf' would become 'android_kernel_lge_hammerhead-caf'
    
    * we detect if a repo is a device tree or not by checking for the
    existence of BoardConfig.mk
    
    Change-Id: I00672adcfaf5e907e99e9ef5ac8d5fbaf741ca02

--- device-deps-regenerator/device2kernel.py
+++ device-deps-regenerator/device2kernel.py
@@ -1,33 +1,21 @@
 import json
-
-# Define device repos that have repos that depend on them,
-# otherwise the script will remove these on the assumption
-# they are common repos
-COMMON_DEVICE = [
-    'android_device_asus_flo',
-    'android_device_asus_grouper',
-    'android_device_google_marlin',
-    'android_device_htc_flounder',
-    'android_device_samsung_espressowifi',
-    'android_device_samsung_n1awifi',
-    'android_device_samsung_t0lte',
-]
-
 with open('out.json') as f:
     mapping = json.load(f)
 
 kernels = {}
 
-reverse_deps = {}
+reverse_deps = {device: [] for device in mapping}
 
 for device in mapping:
-    deps = mapping[device]
-    if device not in reverse_deps:
-        reverse_deps[device] = []
+    deps = mapping[device]['deps']
+
     for repo in deps:
-        if repo['repo'] not in reverse_deps:
-            reverse_deps[repo['repo']] = []
-        reverse_deps[repo['repo']].append(device)
+        name = repo['repo']
+        if len(repo['branch'].split('-')) > 2:
+            name += '-' + '-'.join(repo['branch'].split('-')[2:])
+        if name not in reverse_deps:
+            reverse_deps[name] = []
+        reverse_deps[name].append(device)
 
 def simplify_reverse_deps(repo):
     if len(reverse_deps[repo]) == 0 and '-common' not in repo:
@@ -35,7 +23,7 @@ def simplify_reverse_deps(repo):
     res = set()
     for i in reverse_deps[repo]:
         res.update(simplify_reverse_deps(i))
-    if repo in COMMON_DEVICE:
+    if repo in mapping and not mapping[repo]['common']:
         res.add(repo)
     return res
 
