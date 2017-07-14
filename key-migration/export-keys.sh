96b1c24d5abd8202ecec619813759e91e2aca019
Harry Youd <harry@harryyoud.co.uk>
2017-07-14 14:45:21 +0100

    Add a method to export keys to a usable format
    Change-Id: I8b51f5d718a18991aab9b4231ececd8d129b5fb7

---
+++ key-migration/export-keys.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#CERTS_DIR=~/.android-certs/
+
+CERTS_DIR=/mnt/Android/certs
+
+pushd $CERTS_DIR
+
+for x in platform media shared; do
+    echo ${x}_key_release=\"$(openssl pkcs8 -in $x.pk8 -inform DER -outform PEM -nocrypt | openssl rsa -pubout 2>/dev/null | tail -n+2 | head -n-1 | tr -d '\n')\"
+    echo ${x}_cert_release=\"$(openssl x509 -outform der -in $x.x509.pem | base16)\"
+done
+
+echo release_key=\"$(openssl pkcs8 -in releasekey.pk8 -inform DER -outform PEM -nocrypt | openssl rsa -pubout 2>/dev/null | tail -n+2 | head -n-1 | tr -d '\n')\"
+echo release_cert=\"$(openssl x509 -outform der -in releasekey.x509.pem | base16)\"
+
+popd
