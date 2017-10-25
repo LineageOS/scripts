2c299e02f49c6090eae8d8a5ef0e2a2a28362143
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-24 21:39:21 -0400

    scripts: Add simplified AOSP merger
    Usage: ./lineage/scripts/aosp-merger/aosp-merger.sh [merge|rebase] <oldaosptag> <newaosptag>
    
    Change-Id: I6663e51ee22d7ea262ef634ed5e33c189ee1c2d6

---
+++ aosp-merger/branches_restore.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+if [ ! -e "build/envsetup.sh" ]; then
+    echo "Must run from root of repo"
+    exit 1
+fi
+
+TOP="$PWD"
+BRANCHLIST="$TOP/branches.list"
+
+cat "$BRANCHLIST" | while read l; do
+    set $l
+    PROJECTPATH="$1"
+    BRANCH="$2"
+    cd "$TOP/$PROJECTPATH"
+
+    # Check if we're on this branch already
+    CURBRANCH=$(git status -b --porcelain | head -1 | awk '{print $2}' | sed 's/\.\.\..*//')
+    if [ "$CURBRANCH" == "$BRANCH" ]; then
+        continue
+    fi
+
+    # Sanity check
+    git status | grep "nothing to commit, working directory clean" >/dev/null
+    if [ ${PIPESTATUS[1]} -ne 0 ]; then
+        echo "#!#! Project $PROJECTPATH has uncommitted files, not switching to branch $BRANCH #!#!"
+        exit 1
+    fi
+
+    echo "#### Project $PROJECTPATH Switching to branch $BRANCH ####"
+    git checkout "$BRANCH"
+done
