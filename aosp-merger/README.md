ccde24991f5046c5e3f47d9df33e52ccba744df7
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-24 21:39:21 -0400

    scripts: Add simplified AOSP merger
    Usage: ./lineage/scripts/aosp-merger/aosp-merger.sh [merge|rebase] <oldaosptag> <newaosptag>
    
    Change-Id: I6663e51ee22d7ea262ef634ed5e33c189ee1c2d6

---
+++ aosp-merger/README.md
@@ -0,0 +1,33 @@
+# Rough workflow
+
+1. Snapshot the names of your current working branches to `branches.list` file:
+
+       ./lineage/scripts/aosp-merge/branches_save.sh
+
+2. Note current aosp tag in `.repo/manifests/default.xml`, update it to desired new tag and then create a local commit for the change (aosp-merge script checks for any uncommitted changes in the `.repo/manifests` git repo).
+3. Create a staging branch and merge in the new AOSP tag:
+
+       ./lineage/scripts/aosp-merge/aosp-merge.sh merge \<oldaosptag> \<newaosptag>
+   (where oldaosptag is the original AOSP tag that was in `.repo/manifests/default.xml`)
+   * Example invocation:
+
+         ./lineage/scripts/aosp-merge/aosp-merge.sh merge android-8.0.0_r3 android-8.0.0_r30
+
+4. Every project in your tree should now be one of:
+   * \<newaosptag> if the project was tracking AOSP
+   * a staging branch if the project was a LineageOS fork from AOSP (check `merged_repos.txt` for status and whether there are conflicts to resolve)
+   * the default repo lineage branch for `.repo/manifests/snippets.xml` projects
+5. Restore your local branches and merge in the staging branch:
+
+       ./lineage/scripts/aosp-merge/branches_rebase.sh \<nameofstagingbranch>
+   * Example invocation:
+
+         ./lineage/scripts/aosp-merge/branches_rebase.sh staging/lineage-15.0_merge-android-8.0.0_r30
+6. Build, install, boot, verify, etc.
+
+# TODO
+
+* Make it work for rebase (I'm sure it'll need fixups).
+* Create squashed gerrits for each merge.
+* Abandon squashed gerrits and push each merge automatically.
+* DONE. Instead of merging the staging branch into your local branch (if you have one), create a new branch for the local+staging merge.
---
+++ aosp-merger/branches_rebase.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#####
+# Rebase your local working branches onto a new "upstream" branch.
+# Local branch list is defined in branches.list
+# (and can be created with branches_save.sh)
+# If the upstream branch doesn't exist (eg perhaps in lineage-sdk),
+# simply switch the working branch instead.
+
+if [ ! -e "build/envsetup.sh" ]; then
+    echo "Must run from root of repo"
+    exit 1
+fi
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage ${0} <branch to rebase on top of>"
+    exit 1
+fi
+REBASEONTO="${1}"
+
+TOP="${PWD}"
+BRANCHLIST="${TOP}/branches.list"
+
+cat "${BRANCHLIST}" | while read l; do
+    set ${l}
+    PROJECTPATH="${1}"
+    BRANCH="${2}"
+    NEWBRANCH="${2}-rebase"
+    cd "${TOP}/${PROJECTPATH}"
+
+    # Sanity check
+    [[ -n "$(git status --porcelain)" ]]; then
+        echo -n "!!!! Project ${PROJECTPATH} has uncommitted files, "
+        echo    "not switching to branch ${BRANCH} (skipping) !!!!"
+        continue
+    fi
+
+    # Check the $REBASEONTO branch actually exists
+    git show-ref "refs/heads/${REBASEONTO}" >/dev/null
+    if [ "$?" -ne 0 ]; then
+        # Nope
+        echo -n "#### Project ${PROJECTPATH} branch ${REBASEONTO} does not exist, "
+        echo    "switching to ${BRANCH} instead ####"
+        git checkout "${BRANCH}"
+    else
+        echo "#### Creating ${PROJECTPATH} branch ${NEWBRANCH} from ${BRANCH} ####"
+        repo abandon "${NEWBRANCH}" .
+        repo start "${NEWBRANCH}" .
+        git reset --hard "${BRANCH}"
+        echo -n "#### Project ${PROJECTPATH} Rebasing branch ${NEWBRANCH} "
+        echo    "on top of ${REBASEONTO} ####"
+        git rebase --onto "${REBASEONTO}"
+    fi
+done
