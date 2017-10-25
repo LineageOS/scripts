851475c777f2b9efe4f3770a581a595ec54418cc
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-24 21:39:21 -0400

    scripts: Add simplified AOSP merger
    Usage: ./lineage/scripts/aosp-merger/aosp-merger.sh [merge|rebase] <oldaosptag> <newaosptag>
    
    Change-Id: I6663e51ee22d7ea262ef634ed5e33c189ee1c2d6

---
+++ aosp-merger/aosp-merger.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+usage() {
+    echo "Usage $0 <merge|rebase> <oldaosptag> <newaosptag>"
+}
+
+# Verify argument count
+if [ "$#" -ne 3 ]; then
+    usage
+    exit 1
+fi
+
+OPERATION="$1"
+OLDTAG="$2"
+NEWTAG="$3"
+
+if [ "$OPERATION" != "merge" -a "$OPERATION" != "rebase" ]; then
+    usage
+    exit 1
+fi
+
+# Check to make sure this is being run from the top level repo dir
+if [ ! -e "build/envsetup.sh" ]; then
+    echo "Must be run from the top level repo dir"
+    exit 1
+fi
+
+# Source build environment.
+# Needed for aospremote.
+. build/envsetup.sh
+
+TOP="$ANDROID_BUILD_TOP"
+MERGEDREPOS="$TOP/merged_repos.txt"
+MANIFEST="${TOP}/.repo/manifest.xml"
+BRANCH=$(grep "default revision" "$MANIFEST" | sed 's/^ *//g;s/<default revision=\"refs\/heads\///g;s/\"//g')
+STAGINGBRANCH="staging/${BRANCH}_${OPERATION}-${NEWTAG}"
+
+# Build list of LineageOS forked repos.
+PROJECTPATHS=$(grep "name=\"LineageOS/" "$MANIFEST" | sed -n 's/.*path="\([^"]\+\)".*/\1/p')
+
+echo "#### Old tag = $OLDTAG Branch = $BRANCH Staging branch = $STAGINGBRANCH ####"
+
+# Make sure manifest and forked repos are in a consistent state.
+echo "#### Verifying there are no uncommitted changes on LineageOS forked AOSP projects ####"
+for PROJECTPATH in $PROJECTPATHS .repo/manifests; do
+    cd "$TOP/$PROJECTPATH"
+    git status | grep "nothing to commit, working directory clean" >/dev/null
+    if [ ${PIPESTATUS[1]} -ne 0 ]; then
+        echo "Path $PROJECTPATH has uncommitted changes.  Please fix."
+        exit 1
+    fi
+done
+echo "#### Verification complete - no uncommitted changes found ####"
+
+# Remove any existing list of merged repos file.
+rm -f "$MERGEDREPOS"
+
+# Sync and detach from current branches
+repo sync -d
+
+# Ditch any existing staging branches (across all projects)
+repo abandon "$STAGINGBRANCH"
+
+# Iterate over each forked project
+for PROJECTPATH in $PROJECTPATHS; do
+  cd "$TOP/$PROJECTPATH"
+  repo start "$STAGINGBRANCH" .
+  aospremote | grep -v "Remote 'aosp' created"
+  git fetch -q --tags aosp "$NEWTAG"
+
+  # Check if we've actually changed anything before attempting to merge
+  # If we haven't, just "git reset --hard" to the tag
+  if [[ -z "$(git diff HEAD $OLDTAG)" ]]; then
+    git reset --hard "$NEWTAG"
+    echo -e "reset\t\t$PROJECTPATH" | tee -a "$MERGEDREPOS"
+    continue
+  fi
+
+  # Was there any change upstream?  Skip if not.
+  if [[ -z "$(git diff $OLDTAG $NEWTAG)" ]]; then
+    echo -e "nochange\t\t$PROJECTPATH" | tee -a "$MERGEDREPOS"
+    continue
+  fi
+
+  if [[ "$OPERATION" == "merge" ]]; then
+    echo "#### Merging $NEWTAG into $PROJECTPATH ####"
+    git merge --no-edit --log "$NEWTAG"
+  elif [[ "$OPERATION" == "rebase" ]]; then
+    git rebase "$NEWTAG"
+  else
+    echo "invalid operation"
+    exit
+  fi
+
+  if [[ -n "$(git status --short)" ]]; then
+    echo -e "conflict\t$PROJECTPATH" | tee -a "$MERGEDREPOS"
+  else
+    if [[ "$OPERATION" == "merge" ]]; then
+     echo -e "merged\t\t$PROJECTPATH" | tee -a "$MERGEDREPOS"
+   elif [[ "$OPERATION" == "rebase" ]]; then
+     echo -e "rebased\t\t$PROJECTPATH" | tee -a "$MERGEDREPOS"
+   fi
+  fi
+
+done
---
+++ aosp-merger/branches_save.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+if [ ! -e "build/envsetup.sh" ]; then
+    echo "Must run from root of repo"
+    exit 1
+fi
+
+TOP="$PWD"
+BRANCHLIST="$TOP/branches.list"
+
+# Example repo status output:
+#project build/make/                             branch x
+#project device/huawei/angler/                   branch x
+
+repo status |grep '^project ' | while read l; do
+    set $l
+    PROJECTPATH="$2"
+    BRANCH="$4"
+    echo "$PROJECTPATH $BRANCH"
+done | sort > "$BRANCHLIST"
