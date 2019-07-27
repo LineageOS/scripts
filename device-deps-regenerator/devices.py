d8ebfbfeac4d65194eaddafd345167e6f1313b3d
razorloves <razorloves@gmail.com>
2019-07-26 22:44:23 -0500

    scripts: Add atv to devices.py blacklist
    The device trees with 'atv' in their name are just
    common trees for atv devices. It's not an actual device
    named 'atv' that we build, so it needs to be added to the
    blacklist so that the script doesn't treat it as one.
    
    Change-Id: I2afd840677ef38f510b85d13a93a0ff4c628fa61

--- device-deps-regenerator/devices.py
+++ device-deps-regenerator/devices.py
@@ -5,7 +5,7 @@ with open('out.json') as f:
 
 devices = {}
 suffixes = {}
-blacklist = ['sepolicy', 'devicesettings', 'common']
+blacklist = ['sepolicy', 'devicesettings', 'common', 'atv']
 
 def simplify_reverse_deps(repo, device):
     # repo['branch'] = cm-14.1 or cm-14.1-caf or cm-14.1-sony
