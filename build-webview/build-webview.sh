6d7cb7886e63ecd8a4ad87a6a00c86780117a26e
Kevin F. Haggerty <haggertk@lineageos.org>
2020-01-08 19:36:23 -0700

    Update default webview to 79.0.3945.116
    Change-Id: I3faa822dcc6ff563a28dbc9277fb8d7dd89e0d95

--- build-webview/build-webview.sh
+++ build-webview/build-webview.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-chromium_version="78.0.3904.96"
-chromium_code="3904096"
+chromium_version="79.0.3945.116"
+chromium_code="3945116"
 clean=0
 gsync=0
 supported_archs=(arm arm64 x86 x64)
