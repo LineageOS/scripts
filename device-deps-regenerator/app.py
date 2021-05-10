8d34acdab470882b70f1a74595e04df1599026c8
razorloves <razorloves@gmail.com>
2021-05-10 14:23:02 -0500

    device-deps-regenerator: Drop 16.0
    16.0 builds are no longer available on the download portal.
    
    Change-Id: Ic4ae368b2ca237fd6ca3b346056afe7e585e177e

--- device-deps-regenerator/app.py
+++ device-deps-regenerator/app.py
@@ -20,7 +20,7 @@ print(g.rate_limiting_resettime)
 org = g.get_organization('LineageOS')
 
 # supported branches, newest to oldest
-CUR_BRANCHES = ['lineage-18.1', 'lineage-17.1', 'lineage-16.0']
+CUR_BRANCHES = ['lineage-18.1', 'lineage-17.1']
 
 def get_cm_dependencies(repo):
     branch = None
