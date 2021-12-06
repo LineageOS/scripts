f9f47395f18160e5511ffedfd4cd70914d11c3ae
Chirayu Desai <chirayudesai1@gmail.com>
2021-12-07 03:07:13 +0530

    Import fbpacktool from AOSP
    * qc_image_unpacker doesn't support Pixel 6, this does
    
    Source: https://developers.google.com/android/binary_transparency/pixel#fbpacktool
    Change-Id: If7a409ed9f84e9c8ff43a8aa3d26eaae2c8a0640

---
+++ fbpacktool/fbpack.py
@@ -0,0 +1,85 @@
+# Copyright 2021 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import struct
+from packedstruct import PackedStruct
+from collections import OrderedDict
+
+FBPACK_MAGIC = 0x4b504246   # "FBPK"  FastBook PacK
+FBPACK_VERSION = 2
+FBPACK_DEFAULT_DATA_ALIGN = 16
+
+FBPACK_PARTITION_TABLE = 0
+FBPACK_PARTITION_DATA = 1
+FBPACK_SIDELOAD_DATA = 2
+
+
+class PackEntry(PackedStruct):
+    """Pack entry info."""
+
+    _FIELDS = OrderedDict([
+        ('type', 'I'),
+        ('name', '36s'),
+        ('product', '40s'),
+        ('offset', 'Q'),
+        ('size', 'Q'),
+        ('slotted', 'I'),
+        ('crc32', 'I'),
+    ])
+
+    def __init__(self, type_=0, name=b'', prod=b'', offset=0, size=0, slotted=0, crc32=0):
+        super(PackEntry, self).__init__(type_, name, prod, offset, size, slotted, crc32)
+
+
+class PackHeader(PackedStruct):
+    """ A packed image representation """
+
+    _FIELDS = OrderedDict([
+        ('magic', 'I'),
+        ('version', 'I'),
+        ('header_size', 'I'),
+        ('entry_header_size', 'I'),
+        ('platform', '16s'),
+        ('pack_version', '64s'),
+        ('slot_type', 'I'),
+        ('data_align', 'I'),
+        ('total_entries', 'I'),
+        ('total_size', 'I'),
+    ])
+
+    def __init__(
+            self,
+            magic=FBPACK_MAGIC,
+            version=FBPACK_VERSION,
+            header_size=0,
+            entry_header_size=len(PackEntry()),
+            platform=b'',
+            pack_version=b'',
+            slot_type=0,
+            data_align=FBPACK_DEFAULT_DATA_ALIGN,
+            total_entries=0,
+            total_size=0):
+        super(PackHeader, self).__init__(
+            magic,
+            version,
+            header_size,
+            entry_header_size,
+            platform,
+            pack_version,
+            slot_type,
+            data_align,
+            total_entries,
+            total_size)
+        # update header size once we know all fields
+        self.header_size = len(self)
---
+++ fbpacktool/packedstruct.py
@@ -0,0 +1,61 @@
+# Copyright 2021 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import struct
+
+
+class PackedStruct(object):
+    """Class representing a C style packed structure
+
+    Derived classes need to provide a dictionary with key will be the attributes and the values are
+    the format characters for each field. e.g.
+
+    class Foo(PackedStruct):
+        _FIELDS = {
+            x: 'I',
+            name: '64s',
+        }
+
+    In this case Foo.x will represent an "unsigned int" C value, while Foo.name will be a "char[64]"
+    C value
+
+    """
+
+    def __init__(self, *args, **kwargs):
+        self._fmt = '<' + ''.join(fmt for fmt in self._FIELDS.values())
+        for name in self._FIELDS:
+            setattr(self, name, None)
+
+        for name, val in zip(self._FIELDS.keys(), args):
+            setattr(self, name, val)
+        for name, val in kwargs.items():
+            setattr(self, name, val)
+
+    def __repr__(self):
+        return '{} {{\n'.format(self.__class__.__name__) + ',\n'.join(
+            '    {!r}: {!r}'.format(k, getattr(self, k)) for k in self._FIELDS) + '\n}'
+
+    def __str__(self):
+        return struct.pack(self._fmt, *(getattr(self, x) for x in self._FIELDS))
+
+    def __bytes__(self):
+        return struct.pack(self._fmt, *(getattr(self, x) for x in self._FIELDS))
+
+    def __len__(self):
+        return struct.calcsize(self._fmt)
+
+    @classmethod
+    def from_bytes(cls, data):
+        fmt_str = '<' + ''.join(fmt for fmt in cls._FIELDS.values())
+        return cls(*struct.unpack(fmt_str, data))
