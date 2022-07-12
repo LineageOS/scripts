8f14a1f20fbc68c17cae030bc116d8b15d747de0
Nolen Johnson <johnsonnolen@gmail.com>
2022-07-12 11:41:16 -0400

    scripts: pixel: Make `build-desc-fingerprint.sh` commit changes
    Tests: cd lineage/scripts/pixel && ./build-desc-fingerprint.sh coral flame
    Notes: No additional issues added in `shellcheck`
    Change-Id: I1810ad996d727337b73c6959aaa37bdcaf0d5e5b

--- pixel/build-desc-fingerprint.sh
+++ pixel/build-desc-fingerprint.sh
@@ -64,6 +64,9 @@ main() {
       local mk="$(ls ${top}/device/google/*/lineage_${d}.mk)"
       sed -i "s/${prev_build_id}/${build_id}/g" "${mk}"
       sed -i "s/${prev_build_number}/${build_number}/g" "${mk}"
+      cd "${top}/device/google/${d}"
+      git commit -a -m "Update fingerprint/build description from ${build_id}"
+      cd ../../..
     )
   done
 }
