9f53aa56ef9df669738b1787c973032cc9ed4f8d
LuK1337 <priv.luk@gmail.com>
2023-03-24 18:59:47 +0100

    emoji-updater: Parse emoji-test.txt instead of full-emoji-list.html
    Change-Id: I4744914e077cbe55110cd88b0657d94fd7c86afc

--- emoji-updater/emoji-updater.py
+++ emoji-updater/emoji-updater.py
@@ -3,28 +3,16 @@
 import sys
 
 import requests
-from lxml import etree
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        sys.exit(f'usage: {sys.argv[0]} [url|https://unicode.org/emoji/charts-12.0/full-emoji-list.html]')
+        sys.exit(f'usage: {sys.argv[0]} [url|https://unicode.org/Public/emoji/15.0/emoji-test.txt]')
 
     url = sys.argv[1]
     req = requests.get(url=url)
 
-    parser = etree.HTMLParser(recover=True, encoding='utf-8')
-    doc = etree.fromstring(text=req.content, parser=parser)
-
-    for tr in doc.xpath('.//tr'):
-        mediumhead = tr.xpath('.//th[@class="mediumhead"]/a')
-
-        if len(mediumhead) > 0:
-            print(f'        <!-- {mediumhead[0].text} -->')
-            continue
-
-        code = tr.xpath('.//td[@class="code"]/a')
-
-        if len(code) > 0:
-            codes = ','.join([x[2:] for x in code[0].text.split()])
-            print(f'        <item>{codes}</item>')
-            continue
+    for line in req.text.splitlines():
+        if line.startswith('# subgroup: '):
+            print(f'        <!-- {line.split(maxsplit=2)[-1]} -->')
+        elif '; fully-qualified' in line and not 'skin tone' in line:
+            print(f'        <item>{line.split(";")[0].strip().replace(" ", ",")}</item>')
