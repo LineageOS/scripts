b7d7e76f5f9aa797f84961d70f9937544a649e17
LuK1337 <priv.luk@gmail.com>
2023-03-24 18:59:47 +0100

    emoji-updater: Add "Emoji Presentation Sequences" support
    Change-Id: I4744914e077cbe55110cd88b0657d94fd7c86afc

--- emoji-updater/emoji-updater.py
+++ emoji-updater/emoji-updater.py
@@ -5,6 +5,17 @@ import sys
 import requests
 from lxml import etree
 
+
+def get_emoji_variants():
+    url = 'https://unicode.org/emoji/charts/emoji-variants.html'
+    req = requests.get(url=url)
+
+    parser = etree.HTMLParser(recover=True, encoding='utf-8')
+    doc = etree.fromstring(text=req.content, parser=parser)
+
+    return [ x.attrib['name'] for x in doc.xpath('.//td[@class="code cchars"]/a') ]
+
+
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         sys.exit(f'usage: {sys.argv[0]} [url|https://unicode.org/emoji/charts-12.0/full-emoji-list.html]')
@@ -14,6 +25,8 @@ if __name__ == '__main__':
 
     parser = etree.HTMLParser(recover=True, encoding='utf-8')
     doc = etree.fromstring(text=req.content, parser=parser)
+
+    emoji_variants = get_emoji_variants()
 
     for tr in doc.xpath('.//tr'):
         mediumhead = tr.xpath('.//th[@class="mediumhead"]/a')
@@ -26,5 +39,10 @@ if __name__ == '__main__':
 
         if len(code) > 0:
             codes = ','.join([x[2:] for x in code[0].text.split()])
+
+            # Add Variation Selector-16 to emojis that have default text presentation
+            if codes in emoji_variants:
+                codes += ",FE0F"
+
             print(f'        <item>{codes}</item>')
             continue
