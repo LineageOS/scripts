44b0ae0180d67774592b09e0df4151ccc5dc4829
Michael W <baddaemon87@gmail.com>
2023-10-07 12:20:43 +0200

    scripts: Make REUSE compliant
    Test: pipx run reuse lint
    Change-Id: I73a60c421ec2d4399d511043e92d387dc890eb3d

--- reuse_helper/reuse_helper.py
+++ reuse_helper/reuse_helper.py
@@ -3,6 +3,7 @@
 # SPDX-FileCopyrightText: 2023 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 
+# REUSE-IgnoreStart
 import argparse
 import os
 import re
@@ -197,3 +198,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+# REUSE-IgnoreEnd
