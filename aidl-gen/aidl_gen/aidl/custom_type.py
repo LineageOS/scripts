165a64af603217dc224fae0bd86202d35948a865
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-11-23 02:13:42 +0100

    aidl-gen: Rewrite
    * Support Rust and versioned interfaces
    
    Change-Id: Ibb10deec10ed4e923c1b35676016dc909ba70c95

---
+++ aidl-gen/aidl_gen/aidl/custom_type.py
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from typing import List, Optional
+
+from aidl_gen.aidl.annotation import Annotation
+
+class CustomType:
+    def __init__(
+        self,
+        fqname: str,
+        annotations: Optional[List[Annotation]],
+    ) -> None:
+        self.fqname = fqname
+        self.annotations = annotations or []
+
+        assert "." in self.fqname, f"Name is not fully qualified: {self.fqname}"
