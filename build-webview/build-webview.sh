46995680721031ba40ac9d23820687fa57e7a8f6
Kevin F. Haggerty <haggertk@lineageos.org>
2024-01-25 20:32:47 -0700

    build-webview: Update default webview to 121.0.6167.101
    Change-Id: I2650127440eded93f1592318c9e8ea2ac84ce38b

--- build-webview/build-webview.sh
+++ build-webview/build-webview.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-chromium_version="120.0.6099.230"
-chromium_code="6099230"
+chromium_version="121.0.6167.101"
+chromium_code="6167101"
 clean=0
 gsync=0
 supported_archs=(arm arm64 x86 x64)
