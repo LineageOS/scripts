ca5581965ccfc590f38af8479ad05c46a3f55428
LuK1337 <priv.luk@gmail.com>
2024-02-01 22:31:02 +0100

    shipper: Initial commit
    Change-Id: Iac259c98d31e14fa59ec166f2291bd463b101955

---
+++ shipper/shipper.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+import argparse
+import dataclasses
+import glob
+import pathlib
+import subprocess
+import textwrap
+
+import yaml
+
+
+@dataclasses.dataclass
+class WikiData:
+    codename: str
+    device_names: list
+    maintainers: set
+    versions: list
+
+    def to_jekyll_table(self) -> str:
+        return '| {} |'.format(' | '.join([
+            ' / '.join(self.device_names),
+            f'[{self.codename}](https://wiki.lineageos.org/devices/{self.codename})',
+            ', '.join(self.maintainers),
+            str(self.versions[-2]) if len(self.versions) > 1 else ''
+        ]))
+
+
+def fetch_build_targets(croot: str, git_head: str) -> dict:
+    build_targets = {}
+
+    for line in subprocess.run(['git', 'show', f'{git_head}:lineage-build-targets'],
+                               cwd=f'{croot}/lineage/hudson',
+                               stdout=subprocess.PIPE).stdout.decode().splitlines():
+        if line and not line.startswith("#"):
+            device, build_type, version, cadence = line.split()
+            build_targets[device] = version
+
+    return build_targets
+
+
+def get_wiki_data(croot: str, codename: str) -> WikiData:
+    device_names = []
+    maintainers = []
+    versions = []
+
+    for path in glob.glob(f'{croot}/lineage/wiki/_data/devices/{codename}.yml') + glob.glob(
+            f'{croot}/lineage/wiki/_data/devices/{codename}_variant*.yml'):
+        doc = yaml.load(open(path, 'r').read(), Loader=yaml.FullLoader)
+        device_names.append(f'{doc["vendor"]} {doc["name"]}')
+        maintainers = doc['maintainers']
+        versions = doc['versions']
+
+    return WikiData(codename, device_names, maintainers, versions)
+
+
+def parse_cmdline() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description='Generate device table for blog post')
+    parser.add_argument('hudson_head_before', help='Hudson Git HEAD as of the previous changelog post')
+    return parser.parse_args()
+
+
+def main() -> None:
+    args = parse_cmdline()
+    croot = str(pathlib.Path(__file__).parents[3])
+
+    build_targets_before = fetch_build_targets(croot, args.hudson_head_before)
+    build_targets_after = fetch_build_targets(croot, 'HEAD')
+
+    new_devices = {}
+
+    for branch in sorted(set(build_targets_after.values())):
+        new_devices[branch] = []
+
+    for codename, branch in build_targets_after.items():
+        if build_targets_before.get(codename, None) != branch:
+            new_devices[branch].append(codename)
+
+    for branch, codenames in new_devices.items():
+        _, version = branch.split('-')
+
+        if version.endswith('.0'):
+            version = version[:-2]
+
+        print(textwrap.dedent(f'''\
+            #### Added {version} devices
+
+            {{: .table }}
+            | Device name | Wiki | Maintainers | Moved from |
+            |-------------|------|-------------|------------|'''))
+        print('\n'.join(
+            sorted([get_wiki_data(croot, x).to_jekyll_table() for x in codenames])
+        ))
+        print()
+
+
+if __name__ == '__main__':
+    main()
