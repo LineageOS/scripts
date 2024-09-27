1582093bed04c645761a6002321e47d07074d1a8
Cosmin Tanislav <demonsingur@gmail.com>
2024-09-27 13:11:17 +0300

    pixel: extract-utils: implement V1 image unpacking in fbpack
    Also, while at it, detect radio image automatically.
    
    Change-Id: I28bf371567e208a6bb2113623270cfddefa69f80

--- fbpacktool/fbpack.py
+++ fbpacktool/fbpack.py
@@ -7,11 +7,26 @@ import packedstruct
 
 FBPACK_MAGIC = 0x4b504246   # "FBPK"  FastBook PacK
 FBPACK_VERSION = 2
+FBPACK_VERSION_V1 = 1
 FBPACK_DEFAULT_DATA_ALIGN = 16
 
 FBPACK_PARTITION_TABLE = 0
 FBPACK_PARTITION_DATA = 1
 FBPACK_SIDELOAD_DATA = 2
+
+
+class CommonPackHeader(packedstruct.PackedStruct):
+  magic: int
+  version: int
+  _FIELDS = collections.OrderedDict([
+      ('magic', 'I'),
+      ('version', 'I'),
+  ])
+
+  def __init__(self,
+               magic=FBPACK_MAGIC,
+               version=FBPACK_VERSION_V1):
+    super().__init__(magic, version)
 
 
 class PackEntry(packedstruct.PackedStruct):
@@ -91,3 +106,56 @@ class PackHeader(packedstruct.PackedStruct):
                          total_entries, total_size)
     # update header size once we know all fields
     self.header_size = len(self)
+
+
+class PackHeaderV1(packedstruct.PackedStruct):
+  magic: int
+  version: int
+  img_version: str
+  total_entries: int
+  total_size: int
+  _FIELDS = collections.OrderedDict([
+      ('magic', 'I'),
+      ('version', 'I'),
+      ('img_version', '68s'),
+      ('total_entries', 'I'),
+      ('total_size', 'I'),
+  ])
+
+  def __init__(self,
+               magic=FBPACK_MAGIC,
+               version=FBPACK_VERSION_V1,
+               img_version=b'',
+               total_entries=0,
+               total_size=0):
+    super().__init__(magic, version, img_version, total_entries, total_size)
+
+class PackEntryV1(packedstruct.PackedStruct):
+  type: int
+  name: str
+  size_h: int
+  size: int
+  next_offset_h: int
+  next_offset: int
+  crc32: int
+  _FIELDS = collections.OrderedDict([
+      ('type', 'I'),
+      ('name', '32s'),
+      ('size_h', 'I'),
+      ('size', 'I'),
+      ('next_offset_h', 'I'),
+      ('next_offset', 'I'),
+      ('crc32', 'I'),
+  ])
+
+  # Provide defaults.
+  # pylint: disable=useless-super-delegation
+  def __init__(self,
+               type_=0,
+               name=b'',
+               size_h=0,
+               size=0,
+               next_offset_h=0,
+               next_offset=0,
+               crc32=0):
+    super().__init__(type_, name, size_h, size, next_offset_h, next_offset, crc32)
