cbe999b4aef9951fe5f3c24282053cbe4f8af9e4
Cosmin Tanislav <demonsingur@gmail.com>
2025-01-10 02:12:42 +0200

    update-device-vars: optimize
    Allow passing multiple devices at once to get-new-device-vars.py.
    Do all possible web page requests only once for all devices involved.
    Create soup parsers only once.
    
    Change-Id: I33210c1e406188c85b5aa782909c25fdb4ba2f15

--- pixel/get-new-device-vars.py
+++ pixel/get-new-device-vars.py
@@ -6,6 +6,7 @@
 
 import argparse
 import base64
+from functools import partial
 from bs4 import BeautifulSoup
 from git import cmd
 import os
@@ -24,23 +25,19 @@ BUILD_ID_FILTER = "BUILD_ID="
 SECURITY_PATCH_URL = "https://android.googlesource.com/platform/build/+/refs/{}/core/version_defaults.mk?format=TEXT"
 SECURITY_PATCH_FILTER = "PLATFORM_SECURITY_PATCH :="
 
-def handle_image(html_id):
-    image_html = urllib.request.urlopen(urllib.request.Request(IMAGE_URL, headers=COOKIE)).read()
-    soup = BeautifulSoup(image_html, 'html.parser')
+def handle_image(soup, html_id, output_fn):
     td = soup.find(id=html_id).find_all('td')
     flash_url = td[1].a['href']
     image_url = td[2].a['href']
     image_sha256 = td[3].contents[0]
     build_number = flash_url.split("/")[4].split("?")[0]
-    print('new_build_number="{0}"\nnew_flash_url="{1}"\nnew_image_url="{2}"\nnew_image_sha256="{3}"'.format(build_number, flash_url, image_url, image_sha256))
+    output_fn('new_build_number="{0}"\nnew_flash_url="{1}"\nnew_image_url="{2}"\nnew_image_sha256="{3}"'.format(build_number, flash_url, image_url, image_sha256))
 
-def handle_ota(html_id):
-    ota_html = urllib.request.urlopen(urllib.request.Request(OTA_URL, headers=COOKIE)).read()
-    soup = BeautifulSoup(ota_html, 'html.parser')
+def handle_ota(soup, html_id, output_fn):
     td = soup.find(id=html_id).find_all('td')
     ota_url = td[1].a['href']
     ota_sha256 = td[2].contents[0]
-    print('new_ota_url="{0}"\nnew_ota_sha256="{1}"'.format(ota_url, ota_sha256))
+    output_fn('new_ota_url="{0}"\nnew_ota_sha256="{1}"'.format(ota_url, ota_sha256))
 
 def get_all_aosp_tags(tag_filter):
     all_tags = []
@@ -55,46 +52,80 @@ def get_all_aosp_tags(tag_filter):
     except Exception as e:
         return all_tags
 
-def get_aosp_tag_for_build_id(aosp_tags, wanted_build_id):
+def get_aosp_tags_for_build_ids(aosp_tags):
+    map = {}
     try:
         for aosp_tag in aosp_tags:
             output = base64.decodebytes(urllib.request.urlopen(BUILD_ID_URL.format("tags/" + aosp_tag)).read()).decode()
             for line in output.split('\n'):
                 if BUILD_ID_FILTER in line:
                     found_build_id = line.split("=")[1]
-                    if found_build_id == wanted_build_id:
-                        print('new_aosp_tag="{0}"'.format(aosp_tag))
-                        return aosp_tag
-        print('new_aosp_tag="unknown"')
-        return 'unknown'
+                    map[found_build_id] = aosp_tag
     except Exception as e:
-        print('new_aosp_tag="unknown"')
-        return 'unknown'
+        pass
+
+    return map
 
 def get_security_patch_for_aosp_tag(aosp_tag):
     try:
         output = base64.decodebytes(urllib.request.urlopen(SECURITY_PATCH_URL.format("tags/" + aosp_tag)).read()).decode()
     except:
-        print('new_security_patch=unknown')
-        return
+        return 'unknown'
+
     for line in output.split('\n'):
         if SECURITY_PATCH_FILTER in line:
             security_patch = line.split(":=")[1].strip()
-            print('new_security_patch="{0}"'.format(security_patch))
-            return
-    print('new_security_patch="unknown"')
+            return security_patch
+
+    return 'unknown'
+
+def get_security_patches_for_aosp_tags(aosp_tags):
+    map = {}
+    for aosp_tag in aosp_tags:
+        security_patch = get_security_patch_for_aosp_tag(aosp_tag)
+        map[aosp_tag] = security_patch
+    return map
+
+
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-b', '--build_id', help="Build ID", type=str, required=True)
-    parser.add_argument('-d', '--device', help="Device codename", type=str, required=True)
+    parser.add_argument('--devices', help="Device codenames", type=str, nargs='+', required=True)
+    parser.add_argument('--build-ids', help="Build IDs", type=str, nargs='+', required=True)
+    parser.add_argument('--tmps', help="Temporary files to store device vars into", type=str, nargs='+', required=True)
     parser.add_argument('-t', '--tags_match', default="android-13.0", help='Android version tag to match', type=str)
     args = parser.parse_args()
-    html_id = "{0}{1}".format(args.device, args.build_id.lower())
-    handle_image(html_id)
-    handle_ota(html_id)
-    aosp_tag = get_aosp_tag_for_build_id(get_all_aosp_tags("{0}*".format(args.tags_match)), args.build_id.upper())
-    get_security_patch_for_aosp_tag(aosp_tag)
+
+    assert len(args.devices) == len(args.build_ids) == len(args.tmps)
+
+    image_html = urllib.request.urlopen(urllib.request.Request(IMAGE_URL, headers=COOKIE)).read()
+    image_soup = BeautifulSoup(image_html, 'html.parser')
+
+    ota_html = urllib.request.urlopen(urllib.request.Request(OTA_URL, headers=COOKIE)).read()
+    ota_soup = BeautifulSoup(ota_html, 'html.parser')
+
+    all_aosp_tags = get_all_aosp_tags("{0}*".format(args.tags_match))
+    build_id_aosp_tag_map = get_aosp_tags_for_build_ids(all_aosp_tags)
+    aosp_tag_security_patch_map = get_security_patches_for_aosp_tags(all_aosp_tags)
+
+    def handle_device(device, build_id, output_fn):
+        html_id = "{0}{1}".format(device, build_id.lower())
+        handle_image(image_soup, html_id, output_fn)
+        handle_ota(ota_soup, html_id, output_fn)
+        aosp_tag = build_id_aosp_tag_map.get(build_id, 'unknown')
+        output_fn('new_aosp_tag="{0}"'.format(aosp_tag))
+        security_patch = aosp_tag_security_patch_map.get(aosp_tag, 'unknown')
+        output_fn('new_security_patch="{0}"'.format(security_patch))
+
+    for device, build_id, tmp in zip(args.devices, args.build_ids, args.tmps):
+        with open(tmp, 'w', encoding='utf-8') as f:
+            def output_fns(fs, s):
+                fs.write(s)
+                fs.write('\n')
+
+            output_fn = partial(output_fns, f)
+            handle_device(device, build_id, output_fn)
+
 
 if __name__ == "__main__":
     main()
