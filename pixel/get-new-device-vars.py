2262a3b644b3b264e7cd6ab3ac048653e28d690e
Cosmin Tanislav <demonsingur@gmail.com>
2025-01-10 03:09:55 +0200

    update-device-vars: add support for new security patch location
    Change-Id: Ieb5daab75880dc696565748a3dd7102d8309c214

--- pixel/get-new-device-vars.py
+++ pixel/get-new-device-vars.py
@@ -7,6 +7,7 @@
 import argparse
 import base64
 from functools import partial
+import re
 from bs4 import BeautifulSoup
 from git import cmd
 import os
@@ -24,6 +25,8 @@ BUILD_ID_URL = "https://android.googlesource.com/platform/build/+/refs/{}/core/b
 BUILD_ID_FILTER = "BUILD_ID="
 SECURITY_PATCH_URL = "https://android.googlesource.com/platform/build/+/refs/{}/core/version_defaults.mk?format=TEXT"
 SECURITY_PATCH_FILTER = "PLATFORM_SECURITY_PATCH :="
+NEW_SECURITY_PATCH_URL = "https://android.googlesource.com/platform/build/release/+/refs/tags/{}/flag_values/{}/RELEASE_PLATFORM_SECURITY_PATCH.textproto?format=TEXT"
+NEW_SECURITY_PATCH_FILTER = "string_value: "
 
 def handle_image(soup, html_id, output_fn):
     td = soup.find(id=html_id).find_all('td')
@@ -85,6 +88,35 @@ def get_security_patches_for_aosp_tags(aosp_tags, m):
         m[aosp_tag] = security_patch
 
 
+def get_security_patch(aosp_tag, release):
+    try:
+        url = NEW_SECURITY_PATCH_URL.format(aosp_tag, release)
+        output = base64.decodebytes(urllib.request.urlopen(url).read()).decode()
+    except:
+        return None
+
+    for line in output.split('\n'):
+        if NEW_SECURITY_PATCH_FILTER in line:
+            match = re.search(r'\d{4}-\d{2}-\d{2}', line)
+            security_patch = match.group(0) if match else None
+            return security_patch
+
+    return None
+
+
+def get_security_patches(aosp_tags, build_ids, m):
+    for build_id in build_ids:
+        release, _ = build_id.split('.', 1)
+        release = release.lower()
+
+        for aosp_tag in aosp_tags:
+            security_patch = get_security_patch(aosp_tag, release)
+            if security_patch is None:
+                continue
+
+            m[aosp_tag] = security_patch
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--devices', help="Device codenames", type=str, nargs='+', required=True)
@@ -102,11 +134,14 @@ def main():
     ota_soup = BeautifulSoup(ota_html, 'html.parser')
 
     all_aosp_tags = get_all_aosp_tags("{0}*".format(args.tags_match))
+    build_ids = set(args.build_ids)
+
     build_id_aosp_tag_map = {}
     get_aosp_tags_for_build_ids(all_aosp_tags, build_id_aosp_tag_map)
 
     aosp_tag_security_patch_map = {}
     get_security_patches_for_aosp_tags(all_aosp_tags, aosp_tag_security_patch_map)
+    get_security_patches(all_aosp_tags, build_ids, aosp_tag_security_patch_map)
 
     def handle_device(device, build_id, output_fn):
         html_id = "{0}{1}".format(device, build_id.lower())
