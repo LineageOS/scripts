ae6c17ae51bc09e58b67be074893cc6034a5449c
LuK1337 <priv.luk@gmail.com>
2025-06-22 22:01:22 +0200

    lineage-priv-template: Add Android 16 keys
    Change-Id: I01e45a26a83bf88e5ad5a21e56492f3d8688b7f8

--- lineage-priv-template/keys.mk
+++ lineage-priv-template/keys.mk
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-FileCopyrightText: 2024-2025 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 
 PRODUCT_CERTIFICATE_OVERRIDES := \
@@ -9,12 +9,14 @@ PRODUCT_CERTIFICATE_OVERRIDES := \
     com.android.appsearch.apk:com.android.appsearch.apk.certificate.override \
     com.android.art:com.android.art.certificate.override \
     com.android.bluetooth:com.android.bluetooth.certificate.override \
+    com.android.bt:com.android.bt.certificate.override \
     com.android.btservices:com.android.btservices.certificate.override \
     com.android.cellbroadcast:com.android.cellbroadcast.certificate.override \
     com.android.compos:com.android.compos.certificate.override \
     com.android.configinfrastructure:com.android.configinfrastructure.certificate.override \
     com.android.connectivity.resources:com.android.connectivity.resources.certificate.override \
     com.android.conscrypt:com.android.conscrypt.certificate.override \
+    com.android.crashrecovery:com.android.crashrecovery.certificate.override \
     com.android.devicelock:com.android.devicelock.certificate.override \
     com.android.extservices:com.android.extservices.certificate.override \
     com.android.hardware.authsecret:com.android.hardware.authsecret.certificate.override \
@@ -22,8 +24,16 @@ PRODUCT_CERTIFICATE_OVERRIDES := \
     com.android.hardware.biometrics.fingerprint.virtual:com.android.hardware.biometrics.fingerprint.virtual.override \
     com.android.hardware.boot:com.android.hardware.boot.certificate.override \
     com.android.hardware.cas:com.android.hardware.cas.override \
+    com.android.hardware.contexthub:com.android.hardware.contexthub.certificate.override \
+    com.android.hardware.dumpstate:com.android.hardware.dumpstate.certificate.override \
+    com.android.hardware.gatekeeper.nonsecure:com.android.hardware.gatekeeper.nonsecure.certificate.override \
     com.android.hardware.neuralnetworks:com.android.hardware.neuralnetworks.certificate.override \
+    com.android.hardware.power:com.android.hardware.power.certificate.override \
     com.android.hardware.rebootescrow:com.android.hardware.rebootescrow.certificate.override \
+    com.android.hardware.thermal:com.android.hardware.thermal.certificate.override \
+    com.android.hardware.threadnetwork:com.android.hardware.threadnetwork.override \
+    com.android.hardware.uwb:com.android.hardware.uwb.certificate.override \
+    com.android.hardware.vibrator:com.android.hardware.vibrator.certificate.override \
     com.android.hardware.wifi:com.android.hardware.wifi.certificate.override \
     com.android.healthfitness:com.android.healthfitness.certificate.override \
     com.android.hotspot2.osulogin:com.android.hotspot2.osulogin.certificate.override \
@@ -51,6 +61,7 @@ PRODUCT_CERTIFICATE_OVERRIDES := \
     com.android.telephonymodules:com.android.telephonymodules.certificate.override \
     com.android.tethering:com.android.tethering.certificate.override \
     com.android.tzdata:com.android.tzdata.certificate.override \
+    com.android.uprobestats:com.android.uprobestats.certificate.override \
     com.android.uwb:com.android.uwb.certificate.override \
     com.android.uwb.resources:com.android.uwb.resources.certificate.override \
     com.android.virt:com.android.virt.certificate.override \
