0cc8c318a135398387d7dd48cc9169fe796767c4
Cosmin Tanislav <demonsingur@gmail.com>
2025-07-23 18:19:48 +0300

    scripts: add developer tools
    beautify_rro.py can beautify multiple existing RROs given a directory:
    ./beautify_rro.py /path/to/device/tree/overlays
    
    generate_rro.py can extract RROs from a prebuilt overlays directory:
    ./generate_rro.py /path/to/dump/vendor/overlay
    
    update_certificates.py can update app certificates given a stock dump
    directory.
    ./update_certificates.py -t /path/to/dir/with/keys /path/to/dump
    
    decompile_cil.py can extract sepolicy rules given the path to a prebuilt
    selinux.
    ./decompile_cil.py -k /path/to/kernel -s /path/to/dump/vendor/etc/selinux -o /path/to/output/dir
    
    Change-Id: Ie03e8e3ee378d75b9f4743aeaacef92d562dc2ed

---
+++ dev/bp/bp_utils.py
@@ -0,0 +1,92 @@
+# SPDX-FileCopyrightText: 2025 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+from typing import Dict, List, Optional
+
+ANDROID_BP_NAME = 'Android.bp'
+ANDROID_BP_COPYRIGHT = """
+//
+// SPDX-FileCopyrightText: The LineageOS Project
+// SPDX-License-Identifier: Apache-2.0
+//
+""".lstrip()
+
+
+def merge_bp_module_defaults(base: Dict, override: Dict) -> Dict:
+    result = base.copy()
+
+    for key, value in override.items():
+        if (
+            key in result
+            and isinstance(result[key], list)
+            and isinstance(value, list)
+        ):
+            result[key] = result[key] + value
+        else:
+            result[key] = value
+
+    return result
+
+
+def bp_extend_defaults(
+    module: Dict, defaults: List[str], defaults_map: Dict[str, Dict]
+):
+    base = None
+    missing_defaults = []
+
+    for default_name in defaults:
+        if default_name not in defaults_map:
+            missing_defaults.append(default_name)
+            continue
+
+        defaults_module = defaults_map[default_name]
+
+        if base is None:
+            base = defaults_module
+            continue
+
+        base = merge_bp_module_defaults(base, defaults_module)
+
+    if not base:
+        return module, missing_defaults
+
+    return merge_bp_module_defaults(base, module), missing_defaults
+
+
+def get_partition_specific(partition: Optional[str]):
+    if partition == 'product' or partition == 'system_ext':
+        return f'{partition}_specific'
+    elif partition == 'odm':
+        return 'device_specific'
+    elif partition == 'vendor':
+        return partition
+
+    return None
+
+
+def write_android_bp(apk_path: str, android_bp_path: str, package: str):
+    apk_path_parts = apk_path.split('/')
+
+    partition = None
+    try:
+        overlay_index = apk_path_parts.index('overlay')
+        partition = apk_path_parts[overlay_index - 1]
+    except (ValueError, IndexError):
+        pass
+
+    specific = get_partition_specific(partition)
+    if specific is None:
+        specific = ''
+    else:
+        specific = f'\n    {specific}: true,'
+
+    with open(android_bp_path, 'w') as o:
+        o.write(
+            f'''{ANDROID_BP_COPYRIGHT}
+runtime_resource_overlay {{
+    name: "{package}",{specific}
+}}
+'''
+        )
---
+++ dev/rro/manifest.py
@@ -0,0 +1,84 @@
+# SPDX-FileCopyrightText: 2025 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+from typing import Dict
+
+from lxml import etree
+
+from utils.xml_utils import XML_COMMENT
+
+NAMESPACE_NAME = 'android'
+NAMESPACE = 'http://schemas.android.com/apk/res/android'
+ANDROID_MANIFEST_NAME = 'AndroidManifest.xml'
+PACKAGE_KEY = 'package'
+OVERLAY_TAG = 'overlay'
+TARGET_PACKAGE_KEY = 'targetPackage'
+
+OVERLAY_ATTRS = [
+    'isStatic',
+    'priority',
+    'requiredSystemPropertyName',
+    'requiredSystemPropertyValue',
+]
+
+
+def namespace_attr(attr: str):
+    return f'{{{NAMESPACE}}}{attr}'
+
+
+def parse_package_manifest(manifest_path: str):
+    tree = etree.parse(manifest_path)
+    root = tree.getroot()
+
+    return root.attrib.get(PACKAGE_KEY)
+
+
+def parse_overlay_manifest(manifest_path: str):
+    tree = etree.parse(manifest_path)
+    root = tree.getroot()
+
+    package = root.attrib.get(PACKAGE_KEY)
+    assert package is not None
+
+    overlay_elem = root.find(OVERLAY_TAG)
+    assert overlay_elem is not None
+
+    namespaced_attr = namespace_attr(TARGET_PACKAGE_KEY)
+    target_package = overlay_elem.attrib.get(namespaced_attr)
+
+    overlay_attrs: Dict[str, str] = {}
+
+    for attr in OVERLAY_ATTRS:
+        namespaced_attr = namespace_attr(attr)
+        value = overlay_elem.attrib.get(namespaced_attr)
+        if value is not None:
+            overlay_attrs[attr] = value
+
+    return package, target_package, overlay_attrs
+
+
+def write_manifest(
+    output_path: str,
+    package: str,
+    target_package: str,
+    overlay_attrs: Dict[str, str],
+):
+    with open(output_path, 'w') as o:
+        o.write('<?xml version="1.0" encoding="utf-8"?>')
+        o.write(XML_COMMENT)
+
+        o.write(f'<manifest xmlns:{NAMESPACE_NAME}="{NAMESPACE}"\n')
+        o.write(f'          package="{package}">\n')
+        o.write(
+            f'    <overlay {NAMESPACE_NAME}:{TARGET_PACKAGE_KEY}="{target_package}"'
+        )
+        space = ''
+
+        for attr, value in overlay_attrs.items():
+            o.write(space)
+            o.write(f'\n             {NAMESPACE_NAME}:{attr}="{value}"')
+
+        o.write(' />\n')
+        o.write('</manifest>\n')
