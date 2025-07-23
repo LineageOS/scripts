da64967252bf079b0b5a241ea5dccbd7878463c5
Cosmin Tanislav <demonsingur@gmail.com>
2025-07-23 18:19:48 +0300

    scripts: add developer tools
    beautify_rro.py can beautify multiple existing RROs given a directory:
    ./beautify_rro.py /path/to/device/tree/overlays
    
    generate_rro.py can extract RROs from a prebuilt overlays directory:
    ./generate_rro.py /path/to/dump/vendor/overlay
    
    update_certificates.py can update app certificates given a stock dump
    directory.
    ./update_certificates.py -t /path/to/dir/with/keys /path/to/dump
    
    decompile_cil.py can extract sepolicy rules given the path to a prebuilt
    selinux.
    ./decompile_cil.py -k /path/to/kernel -s /path/to/dump/vendor/etc/selinux -o /path/to/output/dir
    
    Change-Id: Ie03e8e3ee378d75b9f4743aeaacef92d562dc2ed

---
+++ dev/bp/bp_lexer.py
@@ -0,0 +1,79 @@
+# SPDX-FileCopyrightText: 2025 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+import ply.lex as lex
+
+tokens = (
+    'LPAREN',
+    'RPAREN',
+    'LBRACE',
+    'RBRACE',
+    'LBRACK',
+    'RBRACK',
+    'COLON',
+    'COMMA',
+    'STRING',
+    'IDENT',
+    'NUMBER',
+    'PLUS',
+    'EQUALS'
+)
+
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_LBRACE = r'\{'
+t_RBRACE = r'\}'
+t_LBRACK = r'\['
+t_RBRACK = r'\]'
+t_COLON = r':'
+t_COMMA = r','
+t_PLUS = r'\+'
+t_EQUALS = r'='
+
+
+def t_NUMBER(t):
+    r"\d+(\.\d+)?"
+    if '.' in t.value:
+        t.value = float(t.value)
+    else:
+        t.value = int(t.value)
+    return t
+
+
+def t_STRING(t):
+    r'"([^"\\]|\\.)*"'
+    t.value = t.value[1:-1]
+    return t
+
+
+def t_IDENT(t):
+    r"[a-zA-Z_][a-zA-Z0-9_]*"
+    return t
+
+
+t_ignore = ' \t'
+
+
+def t_newline(t):
+    r"\n+"
+    t.lexer.lineno += len(t.value)
+
+
+def t_COMMENT(t):
+    r"//[^\n]*"
+    pass
+
+
+def t_BLOCK_COMMENT(t):
+    r"/\*(.|\n)*?\*/"
+    t.lexer.lineno += t.value.count('\n')
+    pass
+
+
+def t_error(t):
+    raise SyntaxError(f'Illegal character {t.value[0]!r}')
+
+
+bp_lexer = lex.lex()
