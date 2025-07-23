c147ffec67ed27e4d04374977d4964e906378929
Cosmin Tanislav <demonsingur@gmail.com>
2025-07-23 18:19:48 +0300

    scripts: add developer tools
    beautify_rro.py can beautify multiple existing RROs given a directory:
    ./beautify_rro.py /path/to/device/tree/overlays
    
    generate_rro.py can extract RROs from a prebuilt overlays directory:
    ./generate_rro.py /path/to/dump/vendor/overlay
    
    update_certificates.py can update app certificates given a stock dump
    directory.
    ./update_certificates.py -t /path/to/dir/with/keys /path/to/dump
    
    decompile_cil.py can extract sepolicy rules given the path to a prebuilt
    selinux.
    ./decompile_cil.py -k /path/to/kernel -s /path/to/dump/vendor/etc/selinux -o /path/to/output/dir
    
    Change-Id: Ie03e8e3ee378d75b9f4743aeaacef92d562dc2ed

---
+++ dev/decompile_cil.py
@@ -0,0 +1,319 @@
+#!/usr/bin/env python3
+# SPDX-FileCopyrightText: 2025 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+import os
+import shutil
+from argparse import ArgumentParser
+from functools import partial
+from itertools import chain
+from pathlib import Path
+from typing import Dict, List, Optional, Set, Tuple
+
+from sepolicy.cil_rule import CilRule
+from sepolicy.classmap import SELINUX_INCLUDE_PATH, Classmap
+from sepolicy.conditional_type import ConditionalType
+from sepolicy.config import get_default_variables
+from sepolicy.macro import (
+    categorize_macros,
+    decompile_ioctl_defines,
+    decompile_ioctls,
+    decompile_macros,
+    decompile_perms,
+    expand_macro_bodies,
+    macro_conditionals,
+    macro_name,
+    read_macros,
+    resolve_macro_paths,
+    split_macros_text_name_body,
+)
+from sepolicy.match import (
+    RuleMatch,
+    match_macro_rules,
+    merge_class_sets,
+    merge_ioctl_rules,
+    merge_typeattribute_rules,
+    replace_ioctls,
+    replace_macro_rules,
+    replace_perms,
+)
+from sepolicy.match_extract import rule_extract_part_iter
+from sepolicy.output import (
+    group_rules,
+    output_contexts,
+    output_genfs_contexts,
+    output_grouped_rules,
+)
+from sepolicy.rule import RULE_DYNAMIC_PARTS_INDEX, Rule
+from utils.mld import MultiLevelDict
+from utils.utils import Color, android_root, color_print
+
+
+def get_macros_path(version: Optional[str]):
+    system_sepolicy = Path(android_root, 'system/sepolicy')
+    if version is None:
+        return Path(system_sepolicy, 'public')
+
+    return Path(system_sepolicy, f'prebuilts/api/{version}')
+
+
+def print_macro_file_paths(macro_file_paths: List[str]):
+    for macro_path in macro_file_paths:
+        print(f'Loading macros from {macro_path}')
+
+
+def print_variable_ifelse(macros: List[str]):
+    handled_variable_macro_ifelse = [
+        'domain_trans',
+    ]
+
+    # Find conditional variables used in the input text
+    # Conditional variables can be specified, but we need to know if the
+    # macro arguments are used in them
+    for macro in macros:
+        name = macro_name(macro)
+        if name in handled_variable_macro_ifelse:
+            continue
+
+        conditional_variables = macro_conditionals(macro)
+        for conditional_variable in conditional_variables:
+            if conditional_variable.startswith('$'):
+                print(
+                    f'Macro {name} contains variable ifelse: {conditional_variable}'
+                )
+
+
+def rule_arity(rule: Rule):
+    macro_rule_args = rule_extract_part_iter(
+        rule.parts,
+        rule.parts,
+    )
+    assert macro_rule_args is not None
+    return len(macro_rule_args)
+
+
+def sort_macros(macros: List[Tuple[str, List[Rule]]]):
+    # Inside the macro, prefer rules with higher arity to help
+    # the arg matching algorithm
+    for macro in macros:
+        rules = macro[1]
+        rules.sort(key=rule_arity, reverse=True)
+
+
+def decompile_one_cil(
+    cil_path: str,
+    conditional_types_map: Dict[str, ConditionalType],
+    missing_generated_types: Set[str],
+    version: Optional[str],
+):
+    cil_data = Path(cil_path).read_text()
+    cil_lines = cil_data.splitlines()
+
+    genfs_rules: List[Rule] = []
+
+    # Convert lines to rules
+    fn = partial(
+        CilRule.from_line,
+        conditional_types_map=conditional_types_map,
+        missing_generated_types=missing_generated_types,
+        genfs_rules=genfs_rules,
+        version=version,
+    )
+    rules = list(chain.from_iterable(map(fn, cil_lines)))
+
+    return rules, genfs_rules
+
+
+def get_selinux_dir_policy(selinux_dir: str):
+    platform_policy_path = Path(selinux_dir, 'plat_pub_versioned.cil')
+    assert platform_policy_path.exists()
+
+    policy_path = Path(selinux_dir, 'vendor_sepolicy.cil')
+    assert policy_path.exists()
+
+    policy_version_path = Path(selinux_dir, 'plat_sepolicy_vers.txt')
+    assert policy_version_path.exists()
+
+    policy_version = policy_version_path.read_text().strip()
+
+    return str(platform_policy_path), str(policy_path), policy_version
+
+
+def decompile_cil():
+    parser = ArgumentParser(
+        prog='decompile_cil.py',
+        description='Decompile CIL files',
+    )
+    parser.add_argument(
+        '--version',
+        action='store',
+        help='Version string (eg: 31.0)',
+    )
+    parser.add_argument(
+        '--platform',
+        action='store',
+        help='Path to platform policy (eg: vendor/etc/selinux/plat_pub_versioned.cil)',
+    )
+    parser.add_argument(
+        '--policy',
+        action='store',
+        help='Path to policy (eg: vendor/etc/selinux/vendor_sepolicy.cil)',
+    )
+    parser.add_argument(
+        '-s',
+        '--selinux',
+        action='store',
+        help='Path to selinux directory (eg: vendor/etc/selinux)',
+    )
+    parser.add_argument(
+        '-m',
+        '--macros',
+        action='append',
+        default=[],
+        help='Path to directories or files containing macros',
+    )
+    # TODO: allow passing kernel version and download classmap.h
+    parser.add_argument(
+        '-k',
+        '--kernel',
+        action='store',
+        required=True,
+        help='Path to kernel',
+    )
+    parser.add_argument(
+        '-v',
+        '--var',
+        action='append',
+        default=[],
+        help='Variable used when expanding macros',
+    )
+    parser.add_argument(
+        '-o',
+        '--output',
+        action='store',
+        required=True,
+        help='Output directory for the decompiled selinux',
+    )
+
+    args = parser.parse_args()
+
+    macros: List[str] = args.macros
+    output_dir: str = args.output
+    kernel_dir: str = args.kernel
+    selinux_dir: Optional[str] = args.selinux
+
+    if selinux_dir is None:
+        platform_policy: Optional[str] = args.platform
+
+        assert args.policy is not None
+        policy: str = args.policy
+
+        version: Optional[str] = args.version
+    else:
+        platform_policy, policy, version = get_selinux_dir_policy(selinux_dir)
+
+    if not macros:
+        macros_dir_path = str(get_macros_path(version))
+        macros.append(macros_dir_path)
+
+    conditional_types_map: Dict[str, ConditionalType] = {}
+    missing_generated_types: Set[str] = set()
+
+    # Only load generated types from platform policy
+    if platform_policy is not None:
+        _, _ = decompile_one_cil(
+            platform_policy,
+            conditional_types_map,
+            set(),
+            version,
+        )
+
+    rules, genfs_rules = decompile_one_cil(
+        policy,
+        conditional_types_map,
+        missing_generated_types,
+        version,
+    )
+
+    mld: MultiLevelDict[Rule] = MultiLevelDict()
+    for rule in rules:
+        # Add partial matches to this rule
+        # Start partial matching after the first key
+        mld.add(rule.hash_values, rule, RULE_DYNAMIC_PARTS_INDEX)
+
+    macro_file_paths = resolve_macro_paths(args.macros)
+
+    print_macro_file_paths(macro_file_paths)
+
+    input_text, macros_text = read_macros(macro_file_paths)
+
+    print_variable_ifelse(macros_text)
+
+    variables = get_default_variables(mld)
+
+    for kv in args.var:
+        k, v = kv.split('=')
+        variables[k] = v
+
+    expanded_macros_text = expand_macro_bodies(
+        input_text,
+        macros_text,
+        variables,
+    )
+    macros_name_body = split_macros_text_name_body(expanded_macros_text)
+
+    expanded_macros, class_sets, perms, ioctls, ioctl_defines = (
+        categorize_macros(macros_name_body)
+    )
+    decompiled_perms = decompile_perms(perms)
+    decompiled_class_sets = decompile_perms(class_sets)
+    decompiled_ioctls = decompile_ioctls(ioctls)
+    decompiled_ioctl_defines = decompile_ioctl_defines(ioctl_defines)
+
+    # classmap is needed to sort classes and perms to match the compiled
+    # output
+    selinux_include_path = Path(kernel_dir, SELINUX_INCLUDE_PATH).resolve()
+    classmap = Classmap(str(selinux_include_path))
+
+    macros_name_rules = decompile_macros(classmap, expanded_macros)
+
+    sort_macros(macros_name_rules)
+
+    color_print(f'Total rules: {len(mld)}', color=Color.GREEN)
+
+    all_rule_matches: Set[RuleMatch] = set()
+    for name, rules in macros_name_rules:
+        match_macro_rules(
+            mld,
+            name,
+            rules,
+            all_rule_matches,
+        )
+
+    replace_macro_rules(mld, all_rule_matches)
+    merge_typeattribute_rules(mld)
+    merge_ioctl_rules(mld)
+
+    replace_perms(mld, classmap, decompiled_perms)
+    replace_ioctls(mld, decompiled_ioctls, decompiled_ioctl_defines)
+    merge_class_sets(mld, decompiled_class_sets)
+
+    # We can also merge target domains, but rules get long quickly
+    # merge_target_domains(mld)
+
+    color_print(f'Leftover rules: {len(mld)}', color=Color.GREEN)
+
+    grouped_rules = group_rules(mld)
+
+    shutil.rmtree(output_dir, ignore_errors=True)
+    os.makedirs(output_dir)
+
+    output_contexts(selinux_dir, output_dir)
+    output_genfs_contexts(genfs_rules, output_dir)
+    output_grouped_rules(grouped_rules, output_dir)
+
+
+if __name__ == '__main__':
+    decompile_cil()
