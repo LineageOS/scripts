0028268c81334d84ae9c6ee5c9df4ccd97328113
Cosmin Tanislav <demonsingur@gmail.com>
2025-07-23 18:19:48 +0300

    scripts: add developer tools
    beautify_rro.py can beautify multiple existing RROs given a directory:
    ./beautify_rro.py /path/to/device/tree/overlays
    
    generate_rro.py can extract RROs from a prebuilt overlays directory:
    ./generate_rro.py /path/to/dump/vendor/overlay
    
    update_certificates.py can update app certificates given a stock dump
    directory.
    ./update_certificates.py -t /path/to/dir/with/keys /path/to/dump
    
    decompile_cil.py can extract sepolicy rules given the path to a prebuilt
    selinux.
    ./decompile_cil.py -k /path/to/kernel -s /path/to/dump/vendor/etc/selinux -o /path/to/output/dir
    
    Change-Id: Ie03e8e3ee378d75b9f4743aeaacef92d562dc2ed

---
+++ dev/sepolicy/classmap.py
@@ -0,0 +1,75 @@
+# SPDX-FileCopyrightText: 2025 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Dict, List
+
+from utils.utils import split_normalize_text
+
+
+def extract_classmap(access_vectors_path: str) -> Dict[str, List[str]]:
+    text = Path(access_vectors_path).read_text()
+    lines = split_normalize_text(text)
+    text = ''.join(lines)
+    tokens = text.split()
+
+    classes_map: Dict[str, List[str]] = {}
+    commons_map: Dict[str, List[str]] = {}
+
+    i = 0
+    while i < len(tokens):
+        if tokens[i] == 'common':
+            i += 1
+            is_common = True
+        elif tokens[i] == 'class':
+            i += 1
+            is_common = False
+        else:
+            assert False
+
+        name = tokens[i]
+        i += 1
+
+        perms: List[str] = []
+
+        if tokens[i] == 'inherits':
+            i += 1
+
+            assert not is_common
+
+            inherit_name = tokens[i]
+            i += 1
+
+            inherited_perms = commons_map[inherit_name]
+            perms.extend(inherited_perms)
+
+        if tokens[i] == '{':
+            i += 1
+            while tokens[i] != '}':
+                perms.append(tokens[i])
+                i += 1
+            i += 1
+
+        if is_common:
+            assert name not in commons_map
+            commons_map[name] = perms
+        else:
+            assert name not in classes_map
+            classes_map[name] = perms
+
+    return classes_map
+
+
+class Classmap:
+    def __init__(self, access_vectors_path: str):
+        self.__class_perms_map = extract_classmap(access_vectors_path)
+
+    def class_types(self, t: str):
+        for key in self.__class_perms_map:
+            if key.endswith(t):
+                yield key
+
+    def class_perms(self, class_name: str):
+        return self.__class_perms_map[class_name][:]
