3b367bd434978fdd1d8d6f155da450e44e6ab73b
Cosmin Tanislav <demonsingur@gmail.com>
2026-02-19 21:35:20 +0200

    dev: rro: move overlay/package exclusion out of process_rro()
    Change-Id: Ief93eb2c20b2af554d1281ab19cc813f17207dc4

--- dev/rro/process_rro.py
+++ dev/rro/process_rro.py
@@ -36,7 +36,6 @@ from rro.resources import (
 )
 from rro.target_package import (
     find_overlay_android_bp_path_by_name,
-    fixup_target_package,
     get_target_packages,
 )
 from utils.utils import Color, color_print
@@ -85,10 +84,12 @@ def check_rro_matches_aosp(
     if aosp_rro_android_bp_path is None:
         return
 
+    manifest_path = path.join(aosp_rro_android_bp_path, ANDROID_MANIFEST_NAME)
+    aosp_package, aosp_target_package, _ = parse_overlay_manifest(
+        manifest_path,
+    )
+
     (
-        aosp_package,
-        aosp_target_package,
-        _,
         aosp_resources,
         aosp_raw_resources,
         _,
@@ -96,6 +97,8 @@ def check_rro_matches_aosp(
     ) = parse_rro(
         aosp_rro_android_bp_path,
         rro_name,
+        package,
+        target_package,
     )
 
     if package != aosp_package or target_package != aosp_target_package:
@@ -124,6 +127,8 @@ def check_rro_matches_aosp(
 def parse_rro(
     overlay_path: str,
     rro_name: str,
+    package: str,
+    target_package: str,
     manifest_name: str = ANDROID_MANIFEST_NAME,
     resources_dir: str = RESOURCES_DIR,
     all_packages_resources_map: Optional[
@@ -137,8 +142,6 @@ def parse_rro(
     check_matches_aosp: bool = False,
     remove_resources: Optional[Set[Tuple[str | None, str]]] = None,
     keep_packages: Optional[Set[str]] = None,
-    exclude_overlays: Optional[Set[str]] = None,
-    exclude_packages: Optional[Set[str]] = None,
 ):
     if all_packages_resources_map is None:
         all_packages_resources_map = {}
@@ -146,28 +149,8 @@ def parse_rro(
         remove_resources = set()
     if keep_packages is None:
         keep_packages = set()
-    if exclude_overlays is None:
-        exclude_overlays = set()
-    if exclude_packages is None:
-        exclude_packages = set()
 
     manifest_path = path.join(overlay_path, manifest_name)
-
-    package, target_package, overlay_attrs = parse_overlay_manifest(
-        manifest_path,
-    )
-
-    if target_package in exclude_packages:
-        raise ValueError(f'{package}: Excluded by {target_package}')
-    target_package = fixup_target_package(target_package)
-    if target_package in exclude_packages:
-        raise ValueError(f'{package}: Excluded by {target_package}')
-
-    if package in exclude_overlays:
-        raise ValueError(f'{package}: Excluded')
-    package = simplify_rro_package(package)
-    if package in exclude_overlays:
-        raise ValueError(f'{package}: Excluded')
 
     overlay_resources = parse_overlay_resources(
         overlay_path,
@@ -303,9 +286,6 @@ def parse_rro(
         )
 
     return (
-        package,
-        target_package,
-        overlay_attrs,
         resources,
         raw_resources,
         grouped_resources,
@@ -317,6 +297,9 @@ def process_rro(
     input_path: str,
     output_path: str,
     rro_name: str,
+    package: str,
+    target_package: str,
+    overlay_attrs: Dict[str, str],
     manifest_name: str = ANDROID_MANIFEST_NAME,
     resources_dir: str = RESOURCES_DIR,
     all_packages_resources_map: Optional[
@@ -331,14 +314,9 @@ def process_rro(
     check_matches_aosp: bool = False,
     remove_resources: Optional[Set[Tuple[str | None, str]]] = None,
     keep_packages: Optional[Set[str]] = None,
-    exclude_overlays: Optional[Set[str]] = None,
-    exclude_packages: Optional[Set[str]] = None,
     partition: Optional[str] = None,
 ):
     (
-        package,
-        target_package,
-        overlay_attrs,
         _,
         raw_resources,
         grouped_resources,
@@ -346,6 +324,8 @@ def process_rro(
     ) = parse_rro(
         input_path,
         rro_name,
+        package,
+        target_package,
         manifest_name,
         resources_dir,
         all_packages_resources_map,
@@ -354,8 +334,6 @@ def process_rro(
         check_matches_aosp=check_matches_aosp,
         remove_resources=remove_resources,
         keep_packages=keep_packages,
-        exclude_overlays=exclude_overlays,
-        exclude_packages=exclude_packages,
     )
 
     # Preserve existing res/values/*.xml headers BEFORE we delete res/
