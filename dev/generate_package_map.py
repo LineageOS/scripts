375f684eed8db1e2c56a73fb5a0cb5971a4d634b
Cosmin Tanislav <demonsingur@gmail.com>
2026-02-27 02:37:27 +0200

    dev: rro: allow caching package map
    Change-Id: Iaa7e067e9aa3abf74a907cd0f08170262adbc24e

---
+++ dev/generate_package_map.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# SPDX-FileCopyrightText: 2026 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+
+from argparse import ArgumentParser
+from pathlib import Path
+
+from rro.target_package import append_extra_locations, write_package_map
+
+
+def generate_package_map():
+    parser = ArgumentParser(
+        prog='generate_package_map',
+        description='Generate a cache for the package map',
+    )
+    parser.add_argument(
+        'output_path',
+        type=Path,
+    )
+    parser.add_argument(
+        'extra_package_locations',
+        nargs='*',
+    )
+    args = parser.parse_args()
+
+    append_extra_locations(args.extra_package_locations)
+
+    write_package_map(args.output_path)
+
+
+if __name__ == '__main__':
+    generate_package_map()
