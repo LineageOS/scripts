c3bf8fb892c7b20190303b1214bd8af3cd5bd48d
Cosmin Tanislav <demonsingur@gmail.com>
2026-03-10 00:15:09 +0200

    dev: rro: add some verbose output
    Change-Id: Ie79215cd8a7d28887c17efbe12df605396a1d7f7

--- dev/beautify_rro.py
+++ dev/beautify_rro.py
@@ -68,6 +68,7 @@ def remove_shadowed_resources(
     overlays: List[Overlay],
     prefer_resources: DefaultDict[Optional[str], Set[str]],
     devices: Set[Optional[str]],
+    verbose: bool,
 ):
     undetermined_resource_priorities: Dict[
         Tuple[
@@ -94,6 +95,7 @@ def remove_shadowed_resources(
                 overlays=device_overlays,
                 prefer_resources=prefer_resources,
                 device=device,
+                verbose=verbose,
             )
         )
 
@@ -235,6 +237,12 @@ def beautify_rro_main():
         help='Path to cached package map',
         type=Path,
     )
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        help='Print verbose output',
+        action='store_true',
+    )
 
     args = parser.parse_args()
     ignore_packages = cast(str, args.ignore_packages)
@@ -335,6 +343,7 @@ def beautify_rro_main():
         overlays,
         prefer_resources=prefer_resources,
         devices=devices,
+        verbose=args.verbose,
     )
 
     for overlay in overlays:
