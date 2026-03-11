ac6d65c89d3839ad276646f4bddfeb38d4730f7f
Cosmin Tanislav <demonsingur@gmail.com>
2026-03-11 09:38:31 +0200

    dev: rro: parse manifest tree only once
    Change-Id: Ia646bb27338897424f1d628ebb7c3f8158adfb5f

--- dev/rro/overlay.py
+++ dev/rro/overlay.py
@@ -21,6 +21,8 @@ from typing import (
     TypedDict,
 )
 
+from lxml import etree
+
 from bp.bp_module import parse_bp_rro_module
 from bp.bp_utils import (
     ANDROID_BP_NAME,
@@ -65,6 +67,9 @@ def default_dict_optional_str_set_resource() -> DefaultDict[
     return defaultdict(set)
 
 
+ElementTree = etree._ElementTree  # type: ignore
+
+
 @dataclass
 class Overlay:
     name: str
@@ -90,6 +95,8 @@ class Overlay:
     original_name: Optional[str] = None
     original_package: Optional[str] = None
     original_target_package: Optional[str] = None
+
+    manifest_tree: Optional[ElementTree] = None
 
     @property
     def priority(self):
@@ -259,8 +266,9 @@ def parse_overlay_from_android_bp(
     resources_dir = statement.get('resource_dirs', [RESOURCES_DIR])[0]
     resources_path = Path(overlay_path, resources_dir)
 
+    manifest_tree = etree.parse(manifest_path)
     package, target_package, overlay_attrs = parse_overlay_manifest(
-        str(manifest_path),
+        manifest_tree,
     )
 
     original_package = package
@@ -367,6 +375,7 @@ def parse_overlay_from_android_bp(
         original_target_package=original_target_package,
         original_device=original_device,
         preserved_prefixes=preserved_prefixes,
+        manifest_tree=manifest_tree,
     )
 
 
@@ -541,12 +550,11 @@ def remove_overlay_missing_resources(
     if overlay.package_resources is None:
         return
 
-    manifest_path = Path(overlay.path, overlay.manifest_name)
     missing_resources, kept_resources = overlay_resources_remove_missing(
         overlay.package,
         overlay.resources,
         overlay.package_resources,
-        str(manifest_path),
+        overlay.manifest_tree,
         keep_resources,
     )
 
