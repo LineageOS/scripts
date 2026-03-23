967064c706a027859fa59249be0edc145b502b61
Cosmin Tanislav <demonsingur@gmail.com>
2026-03-23 18:51:07 +0200

    Revert "dev: rro: do not write meta after beautification"
    This commit wasn't ready enough.
    
    This reverts commit 2253c5a93bc3612c5c863b4e2235a3ed25c7ef48.
    
    Change-Id: I6d15d15508e18d2d8fc922f2ee472110ad36cad5

--- dev/beautify_rro.py
+++ dev/beautify_rro.py
@@ -196,7 +196,10 @@ def write_beautified_overlay(
         )
         return
 
-    write_overlay(overlay)
+    write_overlay(
+        overlay,
+        write_meta=True,
+    )
 
 
 def beautify_rro_main():
