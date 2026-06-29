852a639b2d4806a356abbce6dfbc0b83c0dd3989
LuK1337 <priv.luk@gmail.com>
2026-06-29 11:50:11 +0200

    repopick: Add --depth=1 for shallow repositories
    Change-Id: I6cd5775acdc324bfac824f47aef26229fb0f06cb

--- repopick/repopick.py
+++ repopick/repopick.py
@@ -641,6 +641,13 @@ def do_git_fetch_pull(args, item):
         cmd = ["git", "pull", "--no-edit"]
     else:
         cmd = ["git", "fetch"]
+
+        is_shallow_repository = subprocess.check_output(
+            ["git", "rev-parse", "--is-shallow-repository"], cwd=project_path
+        )
+
+        if is_shallow_repository == "true":
+            cmd += ["--depth=1"]
     if args.quiet:
         cmd.append("--quiet")
     cmd.extend(["", item["fetch"][method]["ref"]])
