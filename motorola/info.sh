c387e27355cde57c58983f7789b6c1f1d0acc392
Michael Bestas <mkbestas@lineageos.org>
2026-07-25 20:51:37 +0300

    motorola: Update info script to work with OTA zips
    Remove dependency on the RSA txt file and drop unused bootloader
    version while we are at it.
    
    Change-Id: I8f671b021d1dc607f0f5afe102618a7112189933

--- motorola/info.sh
+++ motorola/info.sh
@@ -34,17 +34,44 @@ readonly avbtool="${top}/external/avb/avbtool.py"
 
 ### FUNCTIONS ###
 
+get_prop() {
+  local prop_name="$1"
+  local prop_file="$2"
+
+  if [[ -f "$prop_file" ]]; then
+    grep "^${prop_name}=" "$prop_file" | cut -d'=' -f2- | tr -d '\r' || true
+  fi
+}
+
 info() {
   local source="${1}"
+  local system_prop="${source}/system/build.prop"
+  local system_ext_prop="${source}/system_ext/etc/build.prop"
+  local vendor_prop="${source}/vendor/build.prop"
 
-  local bootloader_version=$(cat ${source}/*.info.txt | grep "MBM Version:" | cut -c 14-)
-  local build_id=$(cat ${source}/*.info.txt | grep "Build Id:" | cut -c 11-)
-  local build_fingerprint=$(cat ${source}/*.info.txt | grep "Build Fingerprint:" | cut -c 20-)
-  local build_description=$(cat ${source}/*.info.txt | grep "Version when read from CPV:" | cut -c 29-)
+  local manufacturer=$(get_prop "ro.product.system.manufacturer" "$system_prop")
+  local name=$(get_prop "ro.product.system.name" "$system_prop")
+  local device=$(get_prop "ro.product.system.device" "$system_prop")
+  local release=$(get_prop "ro.system.build.version.release" "$system_prop")
+  local system_incremental=$(get_prop "ro.system.build.version.incremental" "$system_prop")
+  local type=$(get_prop "ro.system.build.type" "$system_prop")
+  local tags=$(get_prop "ro.system.build.tags" "$system_prop")
+
+  local platform_id=$(get_prop "ro.mot.platform.build_id" "$system_ext_prop")
+
+  local build_id=$(get_prop "ro.vendor.build.id" "$vendor_prop")
+  local vendor_incremental=$(get_prop "ro.vendor.build.version.incremental" "$vendor_prop")
+
   local security_patch=$(python3 ${avbtool} info_image --image ${source}/vbmeta.img | grep "com.android.build.vendor.security_patch" | cut -c 54- | sed s/\'//g)
   local rollback_index=$(python3 ${avbtool} info_image --image ${source}/vbmeta.img | grep "Rollback Index:" | cut -c 27-)
 
-  echo "Bootloader version:    $bootloader_version"
+  local build_fingerprint="${manufacturer}/${name}/${device}:${release}/${build_id}/${system_incremental}-${vendor_incremental}:${type}/${tags}"
+
+  local build_description="${name}-${type} ${release} ${build_id} ${system_incremental}-${vendor_incremental} ${tags}"
+  if [[ -n "$platform_id" ]]; then
+    build_description="${build_description} ${platform_id}"
+  fi
+
   echo "Build ID:              $build_id"
   echo "Build fingerprint:     $build_fingerprint"
   echo "Build description:     $build_description"
